b9073bb2ab6d3213289f10f7a303a3d5063fbd94
rkdwlss2 <rkdwlss2@gmail.com>
2026-06-30 10:52:24 +0900

    cors리펙토링

--- src/main/java/com/example/boardpractice/config/WebConfig.java
+++ src/main/java/com/example/boardpractice/config/WebConfig.java
@@ -1,30 +1,29 @@
 package com.example.boardpractice.config;
 
 import com.example.boardpractice.common.utill.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
-    public WebConfig(LoginUserArgumentResolver loginUserArgumentResolver) {
-        this.loginUserArgumentResolver = loginUserArgumentResolver;
-    }
+
 
     private static final String CORS_URL_PATTERN = "/**";
-    private static final String CORS_URL = "http://127.0.0.1:5500";
+    private final CorsProperties corsProperties;
     private static final String CORS_METHOD = "*";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(CORS_URL_PATTERN)
-                .allowedOrigins(CORS_URL)
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
                 .allowedMethods(CORS_METHOD)
                 .allowCredentials(true);
 
