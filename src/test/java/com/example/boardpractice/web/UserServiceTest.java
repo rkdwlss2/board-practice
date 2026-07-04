e21ea915d22bc56d30a76a5ce235ec7894c20801
rkdwlss2 <rkdwlss2@gmail.com>
2026-07-04 23:42:23 +0900

    유저 컨트롤러 테스트 일부 수정

--- src/test/java/com/example/boardpractice/web/UserServiceTest.java
+++ src/test/java/com/example/boardpractice/web/UserServiceTest.java
@@ -1,4 +1,27 @@
 package com.example.boardpractice.web;
 
+import com.example.boardpractice.repository.UserRepository;
+import com.example.boardpractice.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("회원가입시, 입력한 이메일에 해당하는 유저정보가 있어 true를 리턴해야 된다. 그리고 저장한 유저 정보를 반환해야한다.")
+    public void signupUser() {
+        // given
+
+
+    }
 }
