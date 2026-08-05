41e1ef98bab29439b7db8482c37ee58e369a4316
rkdwlss2 <rkdwlss2@gmail.com>
2026-08-05 13:24:15 +0900

    elastic search url 로그

--- src/main/java/com/example/boardpractice/config/ElasticConfig.java
+++ src/main/java/com/example/boardpractice/config/ElasticConfig.java
@@ -4,18 +4,21 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class ElasticConfig {
     @Value("${spring.elasticsearch.uris}")
     private String esUrl;
     @Bean
     public ElasticsearchClient elasticsearchClient() {
+        log.info("Elasticsearch url: {}", esUrl);
         // 1. Low Level RestClient 생성 (호스트 및 포트 지정)
         RestClient restClient = RestClient.builder(
                 HttpHost.create(esUrl) // 예: http://localhost:9200
